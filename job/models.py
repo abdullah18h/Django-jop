d2d73bae20c328a876d6948a36bac94ebc3795c6
abdullah18h <dzer9181@gmail.com>
2024-06-26 14:19:02 +0200

    some fields

--- job/models.py
+++ job/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+## models.Model inher from it becaus :
+# to get fields 
+
+'''
+django model field
+    -html widget
+    -validation
+    -db.size
+    -
+'''
+class Jop(models.Model): # it like table in database
+
+    j_type=[
+        ('Full Time', 'Full Time'),
+        ('Part Time', 'Part Time')
+    ]
+
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15, choices=j_type)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    exper = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
